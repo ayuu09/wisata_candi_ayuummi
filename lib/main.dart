688a5ce67b558b69fb1d7356c75ea0566c211c90
ayuu09 <ayuummi09@mhs.mdp.ac.id>
2023-10-26 12:02:06 +0700

    WisataCandi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/widgets/ProfileScreen.dart';
 
 
 void main() {
@@ -32,17 +35,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const DetailScreen(),
+      home: ProfileScreen(),
+      //home: DetailScreen(candi: candiList[0]),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
 
